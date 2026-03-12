0345ae5a93751db29ce583be22224b7eaf07329e
nithyaravi8198-tech <nithyaravi8198@gmail.com>
2026-03-12 21:33:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def hello():
+    return "Hello, CI/CD!"
+
 @app.route("/")
 def home():
-    return "Hello from CI/CD Pipeline with Jenkins + SonarQube + Docker!"
+    return hello()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
