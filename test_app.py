caa01face9abc3664673b3073d993e6a2cc14ca8
nithyaravi8198-tech <nithyaravi8198@gmail.com>
2026-03-12 21:35:25 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,8 +3,8 @@ from app import hello
 
 class TestApp(unittest.TestCase):
 
-        def test_hello(self):
-                    self.assertEqual(1,1)
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello from CI/CD Pipeline with Jenkins + SonarQube + Docker!")
 
-                    if __name__ == "__main__":
-                            unittest.main()
+if __name__ == "__main__":
+    unittest.main()
