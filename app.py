eb5658c5e68b56e351c60db313ead4bb275345e8
nithyaravi8198-tech <nithyaravi8198@gmail.com>
2026-03-13 13:31:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 def hello():
-    return "Hello from CI/CD Pipeline with Jenkins + SonarQube!"
+    return "Hello from CI/CD Pipeline with Jenkins + SonarQube + Docker!"
 
 @app.route("/")
 def home():
