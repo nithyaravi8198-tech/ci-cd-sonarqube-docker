93ca7ee0f79787d041312ff06979c935a181968e
nithyaravi8198-tech <nithyaravi8198@gmail.com>
2026-03-13 22:48:57 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import hello
 class TestApp(unittest.TestCase):
 
     def test_hello(self):
-        self.assertEqual(hello(), "CI/CD Jenkins Pipeline Triggered")
+        self.assertEqual(hello(), "CI/CD Pipeline Triggered")
 
 if __name__ == "__main__":
     unittest.main()
